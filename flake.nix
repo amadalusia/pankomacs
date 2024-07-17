b610eb4d5db07ff6bc20b079d36c1c56fe4a51d3
Artur Manuel <balkenix@outlook.com>
2024-07-17 23:49:40 +0100

    flake: formatter
    this was very unformatted LMAO
    
    Signed-off-by: Artur Manuel <balkenix@outlook.com>

--- flake.nix
+++ flake.nix
@@ -16,22 +16,26 @@
       systems = [
         "x86_64-linux"
       ];
+
       flake = {
         overlays.default = final: _prev: import ./overlay final.pkgs;
       };
+
       perSystem = { pkgs, self', config, system, ... }: {
         _module.args.pkgs = import inputs.nixpkgs {
-	  inherit system;
-	  overlays = [
-	    inputs.emacs.overlays.default
-	    inputs.self.overlays.default
-	  ];
-	};
+          inherit system;
+          overlays = [
+            inputs.emacs.overlays.default
+            inputs.self.overlays.default
+          ];
+        };
         packages.pankomacs = pkgs.pankomacs;
-	packages.default = self'.packages.pankomacs;
+        packages.default = self'.packages.pankomacs;
 
-	apps.pankomacs.program = "${self'.packages.pankomacs}/bin/emacs";
-	apps.default = config.apps.pankomacs;
+        apps.pankomacs.program = "${self'.packages.pankomacs}/bin/emacs";
+        apps.default = config.apps.pankomacs;
+
+        formatter = pkgs.nixpkgs-fmt;
       };
     };
 }
